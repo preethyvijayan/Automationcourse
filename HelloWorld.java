2ab3930184ac12228ad60ed775dcfbf22ad0df21
Preethyvijayan <preethivijayan27@gmail.com>
2023-02-04 21:33:09 +0530

    Display hello world

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld// class is a keyword
+{
+public static void main(String args[])// main function - A java program starting and ending in main function
+{
+System.out.println("Hello World");
+}
+}
