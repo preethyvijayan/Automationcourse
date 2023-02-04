e7d24100769452b13c5add2736f3ba19507c6c0a
preethyvijayan <115807568+preethyvijayan@users.noreply.github.com>
2023-02-04 21:48:50 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld// class is a keyword
-{
-public static void main(String args[])// main function - A java program starting and ending in main function
-{
-System.out.println("Hello World");
-}
-}
